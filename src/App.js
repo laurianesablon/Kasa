0b410418022d514c69ea64dc20f6b6b16d7c7be1
Lauriane <109591568+stressedoutdyk3@users.noreply.github.com>
2023-07-09 18:45:12 +0200

    vrai premier commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./components/home/home";
+import Error from "./components/error/error";
+import About from "./components/about/about";
+import Housing from "./components/housing/housing";
+import { BrowserRouter, Routes, Route , Navigate} from "react-router-dom";
+
+
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/error" element={<Error />} />
+          <Route path="*" element={<Navigate to="/error" />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/housing" element={<Housing />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/housing/housing.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Housing() {
+  return (
+    <div>
+      <h1>Housing</h1>
+    </div>
+  );
+}
