a7c306566740ee666c0f75902c7f961ebd9080c9
Lauriane <109591568+stressedoutdyk3@users.noreply.github.com>
2023-09-03 12:40:55 -0400

    retrait des fleches lorsque qu'il y a qu'une seule slide

--- src/components/carrousel.js
+++ src/components/carrousel.js
@@ -1,29 +1,52 @@
-import React, { useEffect, useState, useMemo, useCallback } from 'react';
-import arrowLeft from '../icons/arrow_left.png';
-import arrowRight from '../icons/arrow_right.png';
+import React, { useEffect, useState, useMemo, useCallback } from "react";
+import arrowLeft from "../icons/arrow_left.png";
+import arrowRight from "../icons/arrow_right.png";
 
-function Carrousel({data}) {
+function Carrousel({ data }) {
   const [i, setI] = useState(0);
- 
-  const slides = useMemo(() => [data?.pictures?.map((picture) => picture)].filter(Boolean), [data]);
+
+  const slides = useMemo(
+    () => [data?.pictures?.map((picture) => picture)].filter(Boolean),
+    [data]
+  );
   const slides_length = slides[0]?.length ?? 0;
 
   const handleLeftArrowClick = useCallback(() => {
-    setI((prevI) => (prevI === 0 ? slides_length - 1 : (prevI - 1) % slides_length));
+    setI((prevI) =>
+      prevI === 0 ? slides_length - 1 : (prevI - 1) % slides_length
+    );
   }, [slides_length]);
 
   const handleRightArrowClick = useCallback(() => {
     setI((prevI) => (prevI + 1) % slides_length);
   }, [slides_length]);
-
+  if (slides_length === 1) {
+    return (
+      <div className="carrousel">
+        <img className="carrousel_img" src={slides[0]?.[i]} alt="carrousel" />
+      </div>
+    );
+  }
   return (
     <div className="carrousel">
-      <img className="carrousel_img" src={slides[0]?.[i]} alt="carrousel" /> 
-      <img className="carrousel_arrow_left arrow" alt="left" src={arrowLeft} onClick={handleLeftArrowClick}/>
-      <img className="carrousel_arrow_right arrow" alt="right" src={arrowRight}  onClick={handleRightArrowClick}/>
-      <p className='carrousel_position'>{i+1}/{slides_length}</p>
+      <img className="carrousel_img" src={slides[0]?.[i]} alt="carrousel" />
+      <img
+        className="carrousel_arrow_left arrow"
+        alt="left"
+        src={arrowLeft}
+        onClick={handleLeftArrowClick}
+      />
+      <img
+        className="carrousel_arrow_right arrow"
+        alt="right"
+        src={arrowRight}
+        onClick={handleRightArrowClick}
+      />
+      <p className="carrousel_position">
+        {i + 1}/{slides_length}
+      </p>
     </div>
   );
 }
 
-  export default Carrousel
+export default Carrousel;
